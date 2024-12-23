89d562c553b26332b2c3407cb3b9f5219d9769af
Aleksey <pq1121@mail.ru>
2024-12-23 22:30:52 +0300

    feat(function_menu): add output list all catalog

--- function_menu.py
+++ function_menu.py
@@ -6,3 +6,12 @@ def create_catalog(path):
     path_add = path + rf'\{text},{now},0.rcb'
 
     open(path_add,'w')
+
+def lst_all_catalog(lst: list):
+    if len(lst) != 0:
+        print("Список Каталогов с рецептами")
+        for i in range(len(lst)):
+            lst_new = lst[i].split(',')
+            print(f'{i+1}.{lst_new[0]}')
+    else:
+        print("Каталоги с рецептами отсутствуют")
