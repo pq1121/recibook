e913ada625edb6a4d2785494041488d77b1720bf
Aleksey <pq1121@mail.ru>
2024-12-23 21:43:38 +0300

    feat(main): add num 1 in menu create catalog

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 def origin():
     from os import getlogin, mkdir, system
-
+    import function_menu as f_m
     name = getlogin()
     path = rf'C:\Users\{name}\AppData\Roaming\file'
     try:
@@ -12,10 +12,14 @@ def origin():
 
     while work:
         print('Выбирите нужное действие')
+        print('1.Создание каталога рецептов')
         print('5.Выход')
         num = input()
         system('cls')
-        if num == "5":
+        if num == "1":
+            f_m.create_catalog(path)
+            system('cls')
+        elif num == "5":
             work = False
         else:
             print('Не выбран пункт меню!!!')
