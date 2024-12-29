b22a6691fd6a13cd59d8de444e6c5b13c64d96d0
Aleksey <pq1121@mail.ru>
2024-12-29 15:04:33 +0300

    feat(main): add menu 4 add recipe in catalog

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-
-
 def origin():
-    from os import getlogin, mkdir, system
+    from os import getlogin, mkdir, system, listdir, remove
     import function_menu as f_m
     name = getlogin()
     path = rf'C:\Users\{name}\AppData\Roaming\file'
@@ -17,6 +14,7 @@ def origin():
         print('1.Создание каталога рецептов')
         print('2.Список всех каталогов с рецептами')
         print('3.Удаление каталога')
+        print('4.Добавление рецепта')
         print('5.Выход')
         num = input()
         system('cls')
@@ -24,13 +22,18 @@ def origin():
             f_m.create_catalog(path)
             system('cls')
         elif num == "2":
-            lst_file = os.listdir(path)
+            lst_file = listdir(path)
             f_m.lst_all_catalog(lst_file)
         elif num == "3":
-            lst_file = os.listdir(path)
+            lst_file = listdir(path)
             f_m.lst_all_catalog(lst_file)
             tag = int(input("Введите номер каталога для удаления "))
-            os.remove(f_m.del_catalog(path, lst_file, tag))
+            remove(f_m.del_catalog(path, lst_file, tag))
+        elif num == "4":
+            lst_file = listdir(path)
+            f_m.lst_all_catalog(lst_file)
+            tag_rec = int(input("Выберите номер каталога для добавления рецепта "))
+            f_m.add_recipe(path, lst_file, tag_rec)
         elif num == "5":
             work = False
         else:
